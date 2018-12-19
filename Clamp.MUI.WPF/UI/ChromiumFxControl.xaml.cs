08ecc049b27272b91b8e074ab8f57fa38654c4e5
Aomi <2594636985@qq.com>
2018-12-19 09:25:46 +0800

    增加

---
+++ Clamp.MUI.WPF/UI/ChromiumFxControl.xaml.cs
@@ -0,0 +1,160 @@
+﻿using Chromium.Event;
+using Clamp.MUI.WPF.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Clamp.Explorer.Framework.UI
+{
+    /// <summary>
+    /// ChromiumFxControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ChromiumFxControl : UserControl
+    {
+        private Window _Window;
+        private IntPtr _WindowHandle;
+        private IntPtr _ChromeWidgetHostHandle;
+        private BrowserWidgetMessageInterceptor _ChromeWidgetMessageInterceptor;
+        private Region _DraggableRegion = null;
+        private IntPtr _BrowserHandle;
+        private Matrix _Matrix = new Matrix(1, 0, 0, 1, 0, 0);
+
+        public ChromiumFxControl()
+        {
+            InitializeComponent();
+        }
+
+        private void RequestHandler_OnQuotaRequest(object sender, CfxOnQuotaRequestEventArgs e)
+        {
+            e.SetReturnValue(true);
+        }
+
+        private void DragHandler_OnDraggableRegionsChanged(object sender, Chromium.Event.CfxOnDraggableRegionsChangedEventArgs args)
+        {
+            _DraggableRegion = args.Regions.Aggregate(new Region(), (current, region) =>
+            {
+                var rect = new Rectangle(region.Bounds.X, region.Bounds.Y, region.Bounds.Width, region.Bounds.Height);
+
+                if (region.Draggable)
+                    current.Union(rect);
+                else
+                    current.Exclude(rect);
+
+                return current;
+            });
+
+            _DraggableRegion.Transform(_Matrix);
+        }
+
+        private async void ChromiumWebBrowser_BrowserCreated(object sender, Chromium.WebBrowser.Event.BrowserCreatedEventArgs e)
+        {
+            _BrowserHandle = e.Browser.Host.WindowHandle;
+
+            var resilientGetHandle = new Resilient(() => ChromeWidgetHandleFinder.TryFindHandle(_BrowserHandle, out _ChromeWidgetHostHandle));
+
+            await resilientGetHandle.WithTimeOut(100).StartIn(100);
+            _ChromeWidgetMessageInterceptor = new BrowserWidgetMessageInterceptor(this.ChromiumWebBrowser, _ChromeWidgetHostHandle, OnWebBroswerMessage);
+        }
+
+        private void ChromiumFxControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Loaded -= ChromiumFxControl_Loaded;
+            _Window = Window.GetWindow(this);
+            _WindowHandle = new WindowInteropHelper(_Window).Handle;
+            _Window.Closed += Window_Closed;
+            _Window.LocationChanged += _Window_LocationChanged;
+            _Window.StateChanged += Window_StateChanged;
+            UpdateDpiMatrix();
+        }
+
+        private async void Window_StateChanged(object sender, EventArgs e)
+        {
+            //Do not remove before intensive tests.
+            //When resizing windows, chromium component may not redraw correctly
+            //This fix makes sure that after maximizing the window is correctly redrawn.
+            if (_Window.WindowState == WindowState.Minimized)
+                return;
+
+            await Task.Delay(10);
+            ChromiumWebBrowser.Refresh();
+        }
+
+        private void _Window_LocationChanged(object sender, EventArgs e)
+        {
+            UpdateDpiMatrix();
+        }
+
+        private void UpdateDpiMatrix()
+        {
+            _Matrix = HdiHelper.GetDisplayScaleFactor(_WindowHandle);
+        }
+
+        private bool OnWebBroswerMessage(Message message)
+        {
+            switch (message.Msg)
+            {
+                case NativeMethods.WindowsMessage.WM_LBUTTONDBLCLK:
+                    if (!IsInDragRegion(message))
+                        break;
+
+                    Dispatcher.BeginInvoke(new Action(ToogleMaximize));
+                    return true;
+
+                case NativeMethods.WindowsMessage.WM_LBUTTONDOWN:
+                    if (!IsInDragRegion(message))
+                        return false;
+
+                    NativeMethods.ReleaseCapture();
+                    NativeMethods.PostMessage(_WindowHandle, NativeMethods.WindowsMessage.WM_NCLBUTTONDOWN, (IntPtr)NativeMethods.HitTest.HTCAPTION, IntPtr.Zero);
+                    return true;
+            }
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsInDragRegion(Message message)
+        {
+            if (_DraggableRegion == null)
+                return false;
+
+            var point = GetPoint(message);
+            return _DraggableRegion.IsVisible(point);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static System.Drawing.Point GetPoint(Message message)
+        {
+            var lparam = message.LParam;
+            var x = NativeMethods.LoWord(lparam.ToInt32());
+            var y = NativeMethods.HiWord(lparam.ToInt32());
+            return new System.Drawing.Point(x, y);
+        }
+
+        private void ToogleMaximize()
+        {
+            _Window.WindowState = (_Window.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+            _Window.Closed -= Window_Closed;
+            _Window.LocationChanged -= _Window_LocationChanged;
+            _Window.StateChanged -= Window_StateChanged;
+            _ChromeWidgetMessageInterceptor?.ReleaseHandle();
+            _ChromeWidgetMessageInterceptor?.DestroyHandle();
+            _ChromeWidgetMessageInterceptor = null;
+        }
+
+    }
+}
---
+++ Clamp.MUI.WPF/UI/EmbeddedResourceHandler.cs
@@ -0,0 +1,140 @@
+﻿using Chromium;
+using Chromium.WebBrowser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Clamp.MUI.WPF.UI
+{
+    internal class EmbeddedResourceHandler : CfxResourceHandler
+    {
+        private int readResponseStreamOffset;
+        private Assembly resourceAssembly;
+        private string requestUrl = null;
+        private WebResource webResource;
+        private ChromiumWebBrowser browser;
+        private GCHandle gcHandle;
+
+        private string domain = null;
+
+        internal EmbeddedResourceHandler(Assembly resourceAssembly, ChromiumWebBrowser browser, string domain = null)
+        {
+            this.gcHandle = GCHandle.Alloc(this);
+            this.domain = domain;
+            this.browser = browser;
+            this.resourceAssembly = resourceAssembly;
+            this.GetResponseHeaders += EmbeddedResourceHandler_GetResponseHeaders;
+            this.ProcessRequest += EmbeddedResourceHandler_ProcessRequest;
+            this.ReadResponse += EmbeddedResourceHandler_ReadResponse;
+            this.CanGetCookie += (s, e) => e.SetReturnValue(false);
+            this.CanSetCookie += (s, e) => e.SetReturnValue(false);
+        }
+
+
+        private void EmbeddedResourceHandler_ProcessRequest(object sender, Chromium.Event.CfxProcessRequestEventArgs e)
+        {
+            readResponseStreamOffset = 0;
+
+            CfxRequest request = e.Request;
+            CfxCallback callback = e.Callback;
+
+            Uri uri = new Uri(request.Url);
+
+            this.requestUrl = request.Url;
+
+            var fileName = uri.AbsolutePath;
+
+            if (fileName.StartsWith("/") && fileName.Length > 1)
+            {
+                fileName = fileName.Substring(1);
+            }
+
+            var ass = resourceAssembly;
+            var endTrimIndex = fileName.LastIndexOf('/');
+
+            if (endTrimIndex > -1)
+            {
+                var tmp = fileName.Substring(0, endTrimIndex);
+                tmp = tmp.Replace("-", "_");
+
+                fileName = string.Format("{0}{1}", tmp, fileName.Substring(endTrimIndex));
+            }
+
+            var resourcePath = string.Format("{0}.{1}", uri.Host, fileName.Replace('/', '.'));
+
+            var resourceName = ass.GetManifestResourceNames().SingleOrDefault(p => p.Equals(resourcePath, StringComparison.CurrentCultureIgnoreCase));
+
+            if (!string.IsNullOrEmpty(resourceName) && ass.GetManifestResourceInfo(resourceName) != null)
+            {
+                using (var reader = new System.IO.BinaryReader(ass.GetManifestResourceStream(resourceName)))
+                {
+                    var buff = reader.ReadBytes((int)reader.BaseStream.Length);
+
+                    webResource = new WebResource(buff, MimeHelper.GetMimeType(Path.GetExtension(fileName)));
+
+                    reader.Close();
+
+                    if (!browser.webResources.ContainsKey(requestUrl))
+                    {
+                        browser.SetWebResource(requestUrl, webResource);
+                    }
+                }
+
+                Console.WriteLine($"[加载]:\t{requestUrl}");
+
+                callback.Continue();
+                e.SetReturnValue(true);
+            }
+            else
+            {
+                Console.WriteLine($"[未找到]:\t{requestUrl}");
+
+                callback.Continue();
+                e.SetReturnValue(false);
+            }
+        }
+
+        private void EmbeddedResourceHandler_GetResponseHeaders(object sender, Chromium.Event.CfxGetResponseHeadersEventArgs e)
+        {
+
+            if (webResource == null)
+            {
+                e.Response.Status = 404;
+            }
+            else
+            {
+                e.ResponseLength = webResource.data.Length;
+                e.Response.MimeType = webResource.mimeType;
+                e.Response.Status = 200;
+
+                if (!browser.webResources.ContainsKey(requestUrl))
+                {
+                    browser.SetWebResource(requestUrl, webResource);
+                }
+            }
+        }
+
+
+        private void EmbeddedResourceHandler_ReadResponse(object sender, Chromium.Event.CfxReadResponseEventArgs e)
+        {
+            int bytesToCopy = webResource.data.Length - readResponseStreamOffset;
+            if (bytesToCopy > e.BytesToRead)
+                bytesToCopy = e.BytesToRead;
+            System.Runtime.InteropServices.Marshal.Copy(webResource.data, readResponseStreamOffset, e.DataOut, bytesToCopy);
+            e.BytesRead = bytesToCopy;
+            readResponseStreamOffset += bytesToCopy;
+            e.SetReturnValue(true);
+
+
+            if (readResponseStreamOffset == webResource.data.Length)
+            {
+                gcHandle.Free();
+                Console.WriteLine($"[完成]:\t{requestUrl}");
+            }
+        }
+    }
+}
