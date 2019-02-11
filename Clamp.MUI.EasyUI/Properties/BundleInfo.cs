7b30b2053365330d56e12ea8ff9a49a9e8bafa56
Aomi <2594636985@qq.com>
2019-02-11 17:24:28 +0800

    去掉不需要的项目

--- Clamp.MUI.EasyUI/Properties/BundleInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using Clamp.OSGI.Data.Annotation;
-
-[assembly: Bundle("Clamp.MUI.EasyUI", "1.0.0.0")]
-[assembly: BundleName("admin")]
-[assembly: BundleActivator("Clamp.MUI.EasyUI.EasyUIActivator")]
-[assembly: BundleDependency("Clamp.Linker", "1.0.0.0")]
-[assembly: BundleDependency("Clamp.MUI.Framework", "1.0.0.0")]
-
