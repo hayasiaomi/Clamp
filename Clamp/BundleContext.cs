1569cef8f6d20e8f45f8d60e1fb8f9652f206ff8
Aomi <2594636985@qq.com>
2019-02-12 11:11:00 +0800

    增加注解

--- Clamp/BundleContext.cs
+++ Clamp/BundleContext.cs
@@ -7,12 +7,20 @@ using System.Text;
 
 namespace Clamp
 {
+    /// <summary>
+    /// Bundle的上下文
+    /// </summary>
     public class BundleContext : IBundleContext
     {
         private ClampBundle clampBundle;
 
+        /// <summary>
+        /// 当前上下文所在的执行的Bundle
+        /// </summary>
         public RuntimeBundle RuntimeBundle { private set; get; }
-
+        /// <summary>
+        /// 所在的Bundle
+        /// </summary>
         public Bundle Bundle { get { return this.RuntimeBundle.Bundle; } }
 
         internal BundleContext(RuntimeBundle runtimeBundle, ClampBundle clampBundle)
@@ -20,144 +28,267 @@ namespace Clamp
             this.clampBundle = clampBundle;
             this.RuntimeBundle = runtimeBundle;
         }
-
+        /// <summary>
+        /// 通过ID来获得执行的Bundle
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public RuntimeBundle GetRuntimeBundle(string id)
         {
             return this.clampBundle.GetRuntimeBundle(id);
         }
 
+        /// <summary>
+        /// 通过ID来获得Bundle
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public IBundle GetBundle(string id)
         {
             return this.clampBundle.Registry.GetBundle(id);
         }
-
+        /// <summary>
+        /// 获得所有的Bundle
+        /// </summary>
+        /// <returns></returns>
         public IBundle[] GetBundles()
         {
             return this.clampBundle.Registry.GetBundles();
         }
 
+        /// <summary>
+        /// 通过Name来对应的执行Bundle
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public RuntimeBundle GetRuntimeBundleByName(string name)
+        {
+            return this.clampBundle.GetRuntimeBundleByName(name);
+        }
+
+        #region 扩展功能
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="instanceType"></param>
+        /// <returns></returns>
         public object[] GetExtensionObjects(Type instanceType)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects(instanceType);
         }
 
-        #region 扩展功能
+        /// <summary>
+        ///  获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public T[] GetExtensionObjects<T>()
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects<T>();
         }
 
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="instanceType"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         public object[] GetExtensionObjects(Type instanceType, bool reuseCachedInstance)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects(instanceType, reuseCachedInstance);
         }
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         public T[] GetExtensionObjects<T>(bool reuseCachedInstance)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects<T>(reuseCachedInstance);
         }
 
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public object[] GetExtensionObjects(string path)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects(path);
         }
 
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         public object[] GetExtensionObjects(string path, bool reuseCachedInstance)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects(path, reuseCachedInstance);
         }
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="arrayElementType"></param>
+        /// <returns></returns>
         public object[] GetExtensionObjects(string path, Type arrayElementType)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects(path, arrayElementType);
         }
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public T[] GetExtensionObjects<T>(string path)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects<T>(path);
         }
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="arrayElementType"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         public object[] GetExtensionObjects(string path, Type arrayElementType, bool reuseCachedInstance)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects(path, arrayElementType, reuseCachedInstance);
         }
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         public T[] GetExtensionObjects<T>(string path, bool reuseCachedInstance)
         {
             this.clampBundle.CheckInitialized();
             return this.clampBundle.GetExtensionObjects<T>(path, reuseCachedInstance);
         }
 
-        public RuntimeBundle GetRuntimeBundleByName(string name)
-        {
-            return this.clampBundle.GetRuntimeBundleByName(name);
-        }
         #endregion
 
         #region 注册功能
 
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="instance"></param>
+        /// <returns></returns>
         public RegisterOptions Register(Type registerType, object instance)
         {
             return this.clampBundle.Register(registerType, instance);
         }
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="instance"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public RegisterOptions Register(Type registerType, object instance, string name)
         {
             return this.clampBundle.Register(registerType, instance, name);
         }
 
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="registerImplementation"></param>
+        /// <param name="instance"></param>
+        /// <returns></returns>
         public RegisterOptions Register(Type registerType, Type registerImplementation, object instance)
         {
             return this.clampBundle.Register(registerType, registerImplementation, instance);
         }
 
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="registerImplementation"></param>
+        /// <param name="instance"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public RegisterOptions Register(Type registerType, Type registerImplementation, object instance, string name)
         {
             return this.clampBundle.Register(registerType, registerImplementation, instance, name);
         }
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <param name="resolveType"></param>
+        /// <returns></returns>
         public object Resolve(Type resolveType)
         {
             return this.clampBundle.Resolve(resolveType);
         }
 
-   
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <param name="resolveType"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
         public object Resolve(Type resolveType, ResolveOptions options)
         {
-            return this.clampBundle.Resolve(resolveType,options);
+            return this.clampBundle.Resolve(resolveType, options);
         }
 
-      
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <param name="resolveType"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public object Resolve(Type resolveType, string name)
         {
             return this.clampBundle.Resolve(resolveType, name);
         }
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <typeparam name="ResolveType"></typeparam>
+        /// <returns></returns>
         public ResolveType Resolve<ResolveType>() where ResolveType : class
         {
             return this.clampBundle.Resolve<ResolveType>();
         }
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <typeparam name="ResolveType"></typeparam>
+        /// <param name="options"></param>
+        /// <returns></returns>
         public ResolveType Resolve<ResolveType>(ResolveOptions options) where ResolveType : class
         {
             return this.clampBundle.Resolve<ResolveType>(options);
         }
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <typeparam name="ResolveType"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public ResolveType Resolve<ResolveType>(string name) where ResolveType : class
         {
             return this.clampBundle.Resolve<ResolveType>(name);
--- Clamp/IBundleContext.cs
+++ Clamp/IBundleContext.cs
@@ -7,59 +7,192 @@ using System.Text;
 
 namespace Clamp
 {
+    /// <summary>
+    /// Bundle的上下文
+    /// </summary>
     public interface IBundleContext
     {
+
+        /// <summary>
+        /// 当前上下文所在的执行的Bundle
+        /// </summary>
         RuntimeBundle RuntimeBundle { get; }
 
+        /// <summary>
+        /// 所在的Bundle
+        /// </summary>
         Bundle Bundle { get; }
 
+        /// <summary>
+        /// 通过ID来获得执行的Bundle
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         RuntimeBundle GetRuntimeBundle(string id);
-
+        /// <summary>
+        /// 通过Name来对应的执行Bundle
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         RuntimeBundle GetRuntimeBundleByName(string name);
 
+        /// <summary>
+        /// 通过ID来获得Bundle
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         IBundle GetBundle(string id);
 
+        /// <summary>
+        /// 获得所有的Bundle
+        /// </summary>
+        /// <returns></returns>
         IBundle[] GetBundles();
+
         #region 扩展功能
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="instanceType"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         object[] GetExtensionObjects(Type instanceType, bool reuseCachedInstance);
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         T[] GetExtensionObjects<T>();
-
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         T[] GetExtensionObjects<T>(bool reuseCachedInstance);
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         object[] GetExtensionObjects(string path);
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         object[] GetExtensionObjects(string path, bool reuseCachedInstance);
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="arrayElementType"></param>
+        /// <returns></returns>
         object[] GetExtensionObjects(string path, Type arrayElementType);
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns></returns>
         T[] GetExtensionObjects<T>(string path);
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="arrayElementType"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         object[] GetExtensionObjects(string path, Type arrayElementType, bool reuseCachedInstance);
 
+        /// <summary>
+        /// 获得扩展的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <param name="reuseCachedInstance"></param>
+        /// <returns></returns>
         T[] GetExtensionObjects<T>(string path, bool reuseCachedInstance);
         #endregion
 
         #region 注册功能
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="instance"></param>
+        /// <returns></returns>
         RegisterOptions Register(Type registerType, object instance);
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="instance"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         RegisterOptions Register(Type registerType, object instance, string name);
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="registerImplementation"></param>
+        /// <param name="instance"></param>
+        /// <returns></returns>
         RegisterOptions Register(Type registerType, Type registerImplementation, object instance);
-
+        /// <summary>
+        /// 注册服务对象
+        /// </summary>
+        /// <param name="registerType"></param>
+        /// <param name="registerImplementation"></param>
+        /// <param name="instance"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         RegisterOptions Register(Type registerType, Type registerImplementation, object instance, string name);
-
+        /// <summary>
+        /// 解析服务对象
+        /// </summary>
+        /// <param name="resolveType"></param>
+        /// <returns></returns>
         object Resolve(Type resolveType);
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <param name="resolveType"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
         object Resolve(Type resolveType, ResolveOptions options);
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <param name="resolveType"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
         object Resolve(Type resolveType, string name);
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <typeparam name="ResolveType"></typeparam>
+        /// <returns></returns>
         ResolveType Resolve<ResolveType>() where ResolveType : class;
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <typeparam name="ResolveType"></typeparam>
+        /// <param name="options"></param>
+        /// <returns></returns>
         ResolveType Resolve<ResolveType>(ResolveOptions options) where ResolveType : class;
-
+        /// <summary>
+        ///  解析服务对象
+        /// </summary>
+        /// <typeparam name="ResolveType"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
         ResolveType Resolve<ResolveType>(string name) where ResolveType : class;
 
         #endregion
